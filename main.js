241ab6a07c6601a72c18f05f1bcd013b7fb74a71
Asmodeus <asmodeus.contact@gmail.com>
2020-02-25 00:05:35 +0100

    finished three pages

--- main.js
+++ main.js
@@ -1,3 +1,21 @@
+const burgerToggle = () => {
+  const burger = document.querySelector('.burger-container');
+  const mobileNav = document.querySelector('.mobile-nav');
+  const body = document.querySelector(
+    '.home-main-section, .explore-main-section, .join-main-section'
+  );
+
+  burger.addEventListener('click', () => {
+    mobileNav.classList.add('mobile-nav-visible');
+  });
+
+  body.addEventListener('click', () => {
+    mobileNav.classList.remove('mobile-nav-visible');
+  });
+};
+
+burgerToggle();
+
 $(document).ready(function() {
   $('body').removeClass('preload');
 });
